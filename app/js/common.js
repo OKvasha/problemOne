8840a5e69acd4ced02c89d950a7f111947142167
OKvasha <anuba@bk.ru>
2022-02-19 21:37:33 +0300

    1.0.3

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,13 @@
-$(function () {
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
 
-    // Пользовательские функции
+        const blockID = anchor.getAttribute('href').substr(1)
 
-});
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
